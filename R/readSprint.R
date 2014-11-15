2093a0b67c517bc7a1b0c718095accb84e9707d5
spkal <spk@seapowet.org>
2014-11-15 15:20:40 -0800

    Fixed data. Better plots.

--- R/readSprint.R
+++ R/readSprint.R
@@ -2,5 +2,6 @@
     sprintData <- read.table(file, sep=" ", header=TRUE, stringsAsFactors=FALSE)
     sprintData$Date <- with(sprintData,
 			    as.Date(paste(Year, Month, "15", sep="-"), format="%Y-%b-%d"))
+    sprintData$Name <- as.factor(sprintData$Name)
     sprintData
 }
--- inst/scripts/plotAnytime.R
+++ inst/scripts/plotAnytime.R
@@ -2,4 +2,4 @@ library(dplyr)
 library(ggplot2)
 SprintData %>%
   filter(Service == "Anytime") %>%
-  ggplot(aes(Date, Unit, col=Name)) + geom_line()
+  ggplot(aes(Date, Unit, color=Name)) + geom_line()
